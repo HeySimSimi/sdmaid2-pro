619c41a3ed2d96233a6708642d6f292eaaf51ea5
SimSimi Education Company <sashanurmi@hotmail.com>
2024-10-11 23:16:24 +0300

    Initial tree.

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,20 @@
+object Versions {
+    object Kotlin {
+        const val core = "1.9.25"
+        const val coroutines = "1.8.0"
+    }
+
+    object Dagger {
+        const val core = "2.51.1"
+    }
+
+    object AndroidX {
+        object Navigation {
+            const val core = "2.7.7"
+        }
+    }
+
+    object Desugar {
+        const val core = "2.0.4"
+    }
+}
