8e69d0856dfbe085753e6e00ea14302fbd1a91b2
SimSimi Education Company <sashanurmi@hotmail.com>
2024-10-11 23:11:33 +0300

    Initial tree.

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+    `java-library`
+}
+
+gradlePlugin {
+    plugins {
+        create("projectConfigPlugin") {
+            id = "projectConfig"
+            implementationClass = "ProjectConfigPlugin"
+        }
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+dependencies {
+    implementation("com.android.tools.build:gradle:8.4.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    implementation("com.squareup:javapoet:1.13.0")
+}
